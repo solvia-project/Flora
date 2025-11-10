6c3486c7bc0124e7c132af7cb1a69c2a28a04536
fazaram <fazaramanugraha@gmail.com>
2025-11-10 19:04:13 +0700

    update class

--- resources/views/content/class.blade.php
+++ resources/views/content/class.blade.php
@@ -3,22 +3,87 @@
         <x-navbar />
     </x-slot>
 
-    <section class="min-h-screen flex items-center justify-center bg-center bg-cover"
-             style="background-image: url('{{ asset('img/bg/dashboard.png') }}');">
-        <div class="max-w-7xl mx-auto text-center flex flex-col items-center justify-center px-4 sm:px-6 lg:px-8">
-
-            <h5 class="text-gray-600 mt-3 underline text-xl sm:text-2xl md:text-3xl">
-                Your Floral Journey Begins Here
+    <section class="min-h-screen flex items-center justify-center bg-center bg-cover" style="background: url('{{ asset('img/bg/class.png') }}')">
+        <div class="mx-auto text-center flex flex-col items-center justify-center px-4 sm:px-6 lg:px-8">
+            <h5 class="text-gray-600 underline text-xl sm:text-2xl md:text-3xl mt-20 mb-10">
+                Find the perfect class for your floral journey
             </h5>
-
-            <h1 class="font-['Syne'] text-3xl sm:text-4xl md:text-5xl lg:text-6xl mt-4 px-40">
-                Discover the joy of flower arranging — no experience needed, just your passion!
-            </h1>
-
-            <div class="mt-8">
-                <p class="bg-black text-white py-2 px-6 rounded inline-block text-sm sm:text-base cursor-pointer hover:bg-gray-800 transition">
-                    Book Now
-                </p>
+            <div class="flex flex-col justify-evenly gap-4">
+                {{-- class 1 --}}
+                <div class="flex bg-indigo-200">
+                    <img src="{{ asset('img/class/class1.jpg') }}" alt="" class="w-72 m-10">
+                    <div class="m-10">
+                        <h2>Beginner Floral Arrangement</h2>
+                        <p>IDR 250.000</p>
+                        <div>
+                            <p>A perfect class for beginner who want to learn the basic techniques</p>
+                            <ul>
+                                <li>tes</li>
+                                <li>tes</li>
+                                <li>tes</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="flex flex-col justify-between m-10">
+                        <p>star</p>
+                        <div>
+                            <p>Duration : 2 Hours</p>
+                            <p>Wednesday - at 11.00 or 16.00</p>
+                            <p>Location : Citraland, Surabaya</p>
+                        </div>
+                    </div>
+                </div>
+                {{-- class 1 --}}
+                <div class="flex bg-indigo-200">
+                    <img src="{{ asset('img/class/class2.jpg') }}" alt="" class="w-72 m-10">
+                    <div class="m-10">
+                        <h2>Beginner Floral Arrangement</h2>
+                        <p>IDR 250.000</p>
+                        <div>
+                            <p>A perfect class for beginner who want to learn the basic techniques</p>
+                            <ul>
+                                <li>tes</li>
+                                <li>tes</li>
+                                <li>tes</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="flex flex-col justify-between m-10">
+                        <p>star</p>
+                        <div>
+                            <p>Duration : 2 Hours</p>
+                            <p>Wednesday - at 11.00 or 16.00</p>
+                            <p>Location : Citraland, Surabaya</p>
+                        </div>
+                    </div>
+                </div>
+                {{-- class 1 --}}
+                <div class="flex bg-indigo-200">
+                    <img src="{{ asset('img/class/class3.jpg') }}" alt="" class="w-72 m-10">
+                    <div class="m-10">
+                        <h2>Beginner Floral Arrangement</h2>
+                        <p>IDR 250.000</p>
+                        <div>
+                            <p>A perfect class for beginner who want to learn the basic techniques</p>
+                            <ul>
+                                <li>tes</li>
+                                <li>tes</li>
+                                <li>tes</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="flex flex-col justify-between m-10">
+                        <p>star</p>
+                        <div>
+                            <p>Duration : 2 Hours</p>
+                            <p>Wednesday - at 11.00 or 16.00</p>
+                            <p>Location : Citraland, Surabaya</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="my-10">
+                <button class="bg-black text-white px-6 py-2">Book Now</button>
             </div>
         </div>
     </section>
